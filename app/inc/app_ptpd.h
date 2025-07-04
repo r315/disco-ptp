a5dd656e608e7ac04469c0c7af12b613f2552280
Hugo Reis <38652@alunos.isel.ipl.pt>
2025-07-04 19:53:56 +0100

    ADD: ptpd v2.0.0 source files from: https://github.com/L-KAYA/STM32F429_PTPd

---
+++ app/inc/app_ptpd.h
@@ -0,0 +1,17 @@
+#ifndef APP_PTPD_H
+#define APP_PTPD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "cmsis_os.h"
+
+osThreadId app_ptpd_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PING_CMD_H
